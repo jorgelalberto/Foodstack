938b3a0b6e92f89cc207d4fd09cce2126eddea90
0xedl <122504006+0xedl@users.noreply.github.com>
2023-04-21 11:16:11 -0400

    Add files via upload
    Updated main to include printing a single ingredient's info.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,47 @@
 
 int main() {
     SplayTree splayTree("food.csv");
-
-    int n;
-    cout << "Enter how many Ingredients : " << endl;
-    cin >> n;
+    cout << "Welcome to the Food Finder!" << endl
+    << "Enter an option:\n 1. Search for an ingredient's data\n"
+       "2. Find missing ingredients from a diet\n"
+       "3. Exit\n";
+    int option;
+    cin >> option;
     cin.ignore();
 
-    vector<string> userIngredients;
-    for (int i = 0; i < n; ++i) {
-        string ingredient;
-        getline(cin, ingredient);
-        userIngredients.push_back(ingredient);
+    if (option == 1)
+    {
+        cout << "Enter an ingredient: ";
+        string input;
+        getline(cin, input);
+        FoodData ing = *splayTree.NarrowDownSearch(input);
+        cout << "You've entered: " << ing.Shrt_Desc << endl;
+        std::vector<pair<string, double>> nutrients = ing.GetNutrientValues();
+        for (int i = 0; i < nutrients.size(); i++) {
+            cout << nutrients[i].first << ": " << nutrients[i].second << endl;
+        }
+    }
+    else if (option == 2){
+        int n;
+        cout << "Enter how many Ingredients : " << endl;
+        cin >> n;
+        cin.ignore();
+
+        vector<string> userIngredients;
+        for (int i = 0; i < n; ++i) {
+            string input;
+            getline(cin, input);
+            FoodData ing = *splayTree.NarrowDownSearch(input);
+            cout << "You've entered: " << ing.Shrt_Desc << endl;
+            userIngredients.push_back(ing.Shrt_Desc);
+        }
+
+        cout << "Ingredients entered: \n";
+        for (int i = 0; i < n; i++) {
+            cout << i + 1 << ". " << userIngredients[i] << endl;
+        }
+        splayTree.CalculateFindMissing(userIngredients);
     }
 
-    splayTree.CalculateFindMissing(userIngredients);
     return 0;
 }
